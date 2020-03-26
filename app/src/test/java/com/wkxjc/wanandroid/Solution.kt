dfabb1d9f2ec5fc50258f3fee438ac0d72877808
Kevin Wang <kevin.wang@pb.com>
2020-03-26 18:33:10 +0800

    leetcode

--- app/src/test/java/com/wkxjc/wanandroid/Solution.kt
+++ app/src/test/java/com/wkxjc/wanandroid/Solution.kt
@@ -1,32 +1,52 @@
-class Solution {
+const val X = 26
+const val MODULE = Int.MAX_VALUE
 
-    var result = ""
+class Solution {
+    private var result = ""
+    private val numbers = mutableListOf<Int>()
+    private val hash = mutableListOf<Int>()
+    private val pow = mutableListOf<Int>()
 
     fun longestDupSubstring(S: String): String {
-//        var left = 0
-//        var right = S.length - 1//5
-//        while (left < right) {
-//            val middle = (left + right) / 2 //2
-//            if (valid(S, middle)) left = middle + 1
-//            else right = middle
-//        }
+        initHash(S)
+        var left = 0
+        var right = S.length - 1
+        while (left < right) {
+            val middle = (left + right) / 2
+            if (valid(S, middle)) left = middle + 1
+            else right = middle
+        }
         return result
+    }
+
+    private fun initHash(s: String) {
+        numbers.addAll(s.map { it - 'a' })
+        // hash 存储从 0 开始，长度为 i 的 hash 值
+        hash.add(0)
+        // pow 存储 X 的 i 次方
+        pow.add(1)
+        for (i in 1..s.length) {
+            hash.add(hash[i - 1] * X + numbers[i - 1])
+            pow.add(pow[i - 1] * X)
+        }
     }
 
     // S 能否分割出 middle 长度的重复字符串
     private fun valid(s: String, middle: Int): Boolean {
-        println("middle:$middle")
-        for (i in 0..(s.length - middle)) {//0..3
-            println("i:$i")//1..3
-            for (j in i + 1..(s.length - middle)) {
-                println("j:$j")
-                println("stringI:${s.substring(i, i + middle)},stringJ:${s.substring(j, j + middle)}")
-                if (s.substring(i, i + middle) == s.substring(j, j + middle)) {
-                    result = s.substring(i, i + middle)
-                    println("update result:$result")
-                    return true
-                }
+        val hashSet = HashSet<Int>()
+        var currentHash = hash[middle]
+        hashSet.add(currentHash)
+//        println("current:${s.substring(0, middle)}")
+        for (i in 1..(s.length - middle)) {
+//            println("compare with: ${s.substring(i, i + middle)}")
+            val nextHash = (currentHash - numbers[i - 1] * pow[middle - 1]) * X + numbers[i + middle - 1]
+            if (hashSet.contains(nextHash)) {
+//                println("i = $i, middle = $middle")
+                result = s.substring(i, i + middle)
+                return true
             }
+            hashSet.add(nextHash)
+            currentHash = nextHash
         }
         return false
     }
